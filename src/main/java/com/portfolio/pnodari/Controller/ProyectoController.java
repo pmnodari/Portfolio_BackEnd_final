a0e175e700e8b5c43410bd6e42bbae6134773ce3
pmnodari <pmnodari@gmail.com>
2023-01-03 09:54:20 -0300

    Se corrige nombre ruta proyecto

--- src/main/java/com/portfolio/pnodari/Controller/ProyectoController.java
+++ src/main/java/com/portfolio/pnodari/Controller/ProyectoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/proyecto")
+@RequestMapping("/proyectos")
 @CrossOrigin(origins = "http://localhost:4200/")
 public class ProyectoController {
     
